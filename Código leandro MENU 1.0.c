43f2ea1c177f2be934579a71433f6ddec2034f0d
Rafael Azevedx <55194024+rafael-azevedo21@users.noreply.github.com>
2019-10-22 11:07:05 -0300

    Update Código leandro MENU 1.0.c

--- Código leandro MENU 1.0.c
+++ Código leandro MENU 1.0.c
@@ -2,13 +2,8 @@
 #include<stdio.h>
 #include<math.h>
 #include<locale.h>
-int main()
-{
-	setlocale(LC_ALL, "Portuguese");
-	int num;
-	do
-	{
-		//O que será mostrado ao iniciar o programa
+int menu(){
+		int num;
 		printf("Digite um numero de 0 a 9 \n");
 		printf("[0] Para sair \n" 
 				"[1] Para saber se o numero e par \n" 
@@ -26,129 +21,164 @@ int main()
 				"[13] Para comparar 2 textos e saber se são iguais ou diferentes \n"
 				"[14] Para transferir um texto de uma variável para outra \n"
 				"[15] Para concatenar 2 textaos \n" );
-		//Captar o que o usuário digitou e guardar em num
+						//Captar o que o usuário digitou e guardar em num
 		scanf("%d", &num);
+		return(num);
+
+		
+}
+
+void par(){
+		//O objetivo aqui era saber se o núemro era par, ou seja divisivel por 2
+	int num2;
+	int resto;
+	printf("Digite um numero inteiro: \n");
+	scanf("%d", &num2);
+	resto=num2%2;
+	if(resto == 0)
+	//Se ele for par, a divisão dele por 2 terá resto 0, caso contrário ele será impar
+	{
+		printf("Esse numero e par \n");
+	}else if(resto !=0)
+		printf("Esse numero e impar \n");
+}
+
+void potencia(){
+	//Nesse, o objetivo era calcular a potenciação de um número de ponto flutuante elevado a um inteiro
+	float numA, resultado;
+	int numB;
+	printf("Insira um numero de ponto flutuante: ");
+	scanf("%f", &numA);
+	printf("Insira um numero inteiro: ");
+	scanf("%d", &numB);
+	resultado = pow(numA, numB);
+	//Para realizar essa função, foi utilizado o comando pow introduzido pela biblioteca math
+	printf("Valor da potência: 	%.3e \n", resultado );
+}
+
+void raiz(){
+	/*Nesse, o objetivo era calcular a raiz de um número de ponto flutuante
+	tendo como índice um númeor inteiro*/
+	float numA1, resultado1;
+	int numB1;
+	printf("Insira um numero de ponto flutuante: ");
+	scanf("%f", &numA1);
+	printf("Insira um numero inteiro: ");
+	scanf("%d", &numB1);
+	resultado1 = pow(numA1, 1./numB1);
+	//Para isso, utilizei o mesmo comando pow, usando uma das  propriedades de potenciação.
+	printf("Valor da raiz: 	%.3e \n", resultado1);
+}
+
+void bissexto(){
+	//Aqui, o objetivo do programa era ler um ano dado pelo usuário e informar se ele é bissexto ou não
+	int ano,bissexto;
+	printf(" Digite um ano para descobrir se ele é bissexto: ");
+	scanf("%d", &ano);
+	bissexto = ano%4;
+	/*Sabendo que todo ano bissexto é multiplo de 4,
+	 sua divisão por 4 tem que ter como resto 0, caso dê algo diferente disso, não é bissexto.*/
+	if(bissexto==0){
+		printf("O ano é bissexto \n");
+	}
+	else{
+		printf("O ano não é bissexto \n");
+	}
+}
+
+void mediaPond(){
+	/*O objetivo desse case era calcular a média ponderada de 3 notas.
+ 	Onde as duas primerias tem peso 1 e a terceira tem peso 3*/
+	float nota1,nota2,nota3,media;
+	printf ("Digite sua primeira nota");
+	scanf("%f", &nota1);
+	printf ("Digite sua segunda nota");
+	scanf("%f", &nota2);
+	printf("Digite sua terceita nota");
+	scanf("%f", &nota3);
+	media=(nota1+nota2+2*nota3)/4;
+	/*Para realizar tal função, o programa recebe os valores digitados pelo usuário,
+	 multiplica pelos respectivos pesos e divide pelo número total deldes.*/
+	if(media>= 6){
+		printf("Sua media e %f e voce foi aprovado \n", media);
+	}
+	else{
+		printf("Sua media e %f e voce foi reprovado \n", media);
+	}
+}
+
+void media(){
+	//Nesse programa, o objetivo além de calcular a média é verificar se as notas digitadads são válidas
+	float nota1, nota2,media;
+	printf("Digite sua primeira nota: ");
+	scanf("%f", &nota1);
+	if (nota1>=0 && nota1<=10)
+	/*Tomando a nota minima como 0 e a máxima como 10, tem-se essa estrutura if
+	, caso o usuário digite uma nota inválida, encerra-se o case e se reexibe o menu*/
+	{
+		printf("Digite sua Segunda nota: ");
+		scanf("%f", &nota2);
+		if (nota2 >= 0 && nota2 <= 10)
+		{
+			media= (nota1 + nota2)/2;
+			printf("Sua média é: %f \n", media);
+		}
+		else{
+			printf("Nota inválida \n");
+		}
+	}
+	else
+	printf("Nota inválida \n");
+}
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	//Esse num é outra variável em comparação com a da função menu.
+	int num;
+	do
+	{
+		num=menu();
 		//Para limpar o cache
 		fflush(stdin);
 		//Comando importado pela biblioteca stdlib, serve para limpar a tela do programa
 		system("CLS");
-		
 		if(num>0 && num<16)
 		{
 			switch (num)
 				{
 					case 1 :
 					{
-						//O objetivo aqui era saber se o núemro era par, ou seja divisivel por 2
-						int num2;
-						int resto;
-						printf("Digite um numero inteiro: \n");
-						scanf("%d", &num2);
-						resto=num2%2;
-						if(resto == 0)
-						//Se ele for par, a divisão dele por 2 terá resto 0, caso contrário ele será impar
-					{
-						printf("Esse numero e par \n");
-					}else if(resto !=0)
-						printf("Esse numero e impar \n");
-					break;
+						//Chama a função par
+						par();
+						break;
 					}
 					
 					case 2 :
 					{
-						//Nesse, o objetivo era calcular a potenciação de um número de ponto flutuante elevado a um inteiro
-						float numA, resultado;
-						int numB;
-						printf("Insira um numero de ponto flutuante: ");
-						scanf("%f", &numA);
-						printf("Insira um numero inteiro: ");
-						scanf("%d", &numB);
-						resultado = pow(numA, numB);
-						//Para realizar essa função, foi utilizado o comando pow introduzido pela biblioteca math
-						printf("Valor da potência: 	%.3e \n", resultado );
+						potencia();
 						break;
 					}
 					case 3 :
 					{
-						/*Nesse, o objetivo era calcular a raiz de um número de ponto flutuante
-						 tendo como índice um númeor inteiro*/
-						float numA1, resultado1;
-						int numB1;
-						printf("Insira um numero de ponto flutuante: ");
-						scanf("%f", &numA1);
-						printf("Insira um numero inteiro: ");
-						scanf("%d", &numB1);
-						resultado1 = pow(numA1, 1./numB1);
-						//Para isso, utilizei o mesmo comando pow, usando uma das  propriedades de potenciação.
-						printf("Valor da raiz: 	%.3e \n", resultado1);
+						raiz();
 						break;
 					}
 					
 					case 4 :
 					{
-						//Aqui, o objetivo do programa era ler um ano dado pelo usuário e informar se ele é bissexto ou não
-						int ano,bissexto;
-						printf(" Digite um ano para descobrir se ele é bissexto: ");
-						scanf("%d", &ano);
-						bissexto = ano%4;
-						/*Sabendo que todo ano bissexto é multiplo de 4,
-						 sua divisão por 4 tem que ter como resto 0, caso dê algo diferente disso, não é bissexto.*/
-						if(bissexto==0){
-							printf("O ano é bissexto \n");
-						}
-						else{
-							printf("O ano não é bissexto \n");
-						}
+						bissexto();
 						break;
 					}
 					
 					case 5 :
 					{
-						/*O objetivo desse case era calcular a média ponderada de 3 notas.
-					 	Onde as duas primerias tem peso 1 e a terceira tem peso 3*/
-						float nota1,nota2,nota3,media;
-						printf ("Digite sua primeira nota");
-						scanf("%f", &nota1);
-						printf ("Digite sua segunda nota");
-						scanf("%f", &nota2);
-						printf("Digite sua terceita nota");
-						scanf("%f", &nota3);
-						media=(nota1+nota2+2*nota3)/4;
-						/*Para realizar tal função, o programa recebe os valores digitados pelo usuário,
-						 multiplica pelos respectivos pesos e divide pelo número total deldes.*/
-						if(media>= 6){
-							printf("Sua media e %f e voce foi aprovado \n", media);
-						}
-						else{
-							printf("Sua media e %f e voce foi reprovado \n", media);
-						}
+						mediaPond();
 						break;
 					}
 					
 					case 6 :
 					{
-						//Nesse programa, o objetivo além de calcular a média é verificar se as notas digitadads são válidas
-						float nota1, nota2,media;
-						printf("Digite sua primeira nota: ");
-						scanf("%f", &nota1);
-						if (nota1>=0 && nota1<=10)
-						/*Tomando a nota minima como 0 e a máxima como 10, tem-se essa estrutura if
-						, caso o usuário digite uma nota inválida, encerra-se o case e se reexibe o menu*/
-						{
-							printf("Digite sua Segunda nota: ");
-							scanf("%f", &nota2);
-							if (nota2 >= 0 && nota2 <= 10)
-							{
-								media= (nota1 + nota2)/2;
-								printf("Sua média é: %f \n", media);
-							}
-							else{
-								printf("Nota inválida \n");
-								break;
-							}
-						}
-						else
-						printf("Nota inválida \n");
+						media();
 						break;
 					}
 			
